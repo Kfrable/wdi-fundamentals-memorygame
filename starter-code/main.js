43cc62c2668b11baa66f99ede8f21a2e875a539e
Kfrable <kcfrable@gmail.com>
2017-03-19 16:21:52 -0400

    updatedgame logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,56 +1,64 @@
 console.log("JS file is connected to HTML! Woo!")
 
+
 var cards = ['queen', 'queen', 'king', 'king'];
-var cardsInPlay = [ ];
+var cardsInPlay = [];
 var gameBoard = document.getElementById('game-board');
 
 
 function createCards() {
 	for (var i=0; i<cards.length; i++) {
-
+		
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
 		gameBoard.appendChild(cardElement);
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
 	}
 }
 createCards();
 
-function craeteBoard() {
-	for (var i = 0: i < cards.length: i++) {
-		cardElement.setAttribute('data-card' i);
-		cardElement.addEventListener('click',isTwoCards);
 
-
-	}
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	cardElement.addEventListener('click', isTwoCards);
+	cardElement.setAttribute('data-card', cards[i]);
+					
 }
+}
+
+function isMatch() {
+	if ( cardsInPlay [0] === cardsInPlay [1]) {
+	alert ("You Got A Match!");
+	} else  {
+	alert("Sorry,Try Again.");
+	}
+	
+	
+	
+}
+
+
 function isTwoCards() {
 	cardsInPlay.push(this.getAttribute('data-card'));
-	if(cardsInPlay.length === 2) {
+	if (this.getAttribute('data-card') === "king") {
+		this.innerHTML = '<img src = "images/king.png" alt="King of Diamonds" />'
+	} else {
+		this.innerHTML = '<img src = "images/queen.png" alt="Queen of Clubs" />'
+	}
+
+
+	if (cardsInPlay.length === 2) {
 		isMatch(cardsInPlay);
 		cardsInPlay = [];
+		this.innerHTML = " ";
+		location.reload();
+
+		
 	}
 }
 
-function isMatch(cards){
-	if(cards[0] === cards[1]){
-		alert('you have a match!');
-	} else {
-		alert('sorry, not a match!');
-	  
-	}
-}
+createBoard();
 
-
-/* if (cardOne === cardTwo) {
-	alert ("You have a match");
-}else if (cardFour === cardThree) {
-	alert("You have a match");
-}else if (cardOne === cardThree) {
-	alert("Not a match");
-}else if (cardOne === cardFour) {
-	alert("Not a match");
-}else if (cardTwo === cardThree) {
-	alert("Not a match");
-}else if (cardTwo === cardFour) {
-	alert("Not a match");
-}*/
